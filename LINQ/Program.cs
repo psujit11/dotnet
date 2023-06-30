b3f20b030a60e9b3994738214ebdf0e5042bb1a0
sujit poudel <poudelsujit9@gmail.com>
2023-07-01 00:05:12 +0545

    made this work with proper output

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace LINQ// Note: actual namespace depends on the project name.
 {
@@ -8,40 +9,12 @@ namespace LINQ// Note: actual namespace depends on the project name.
     {
         static void Main()
         {
-            //LINQ - Language INtegrated Query
-            int [] numbers ={12,34,25,36,99,35,32,23,77,76,57,54,43,78,89,98,98,98};
-            // Method syntax (lambda expression)
-            var result1 =numbers.Where(num => num > 50).Select(num => num);
-            // Query expression syntax
-            var result2 = from num in numbers
-                            where num > 50
-                            select num;
-            
-            //Restrictions - where 
-
-            var result3 = from num in numbers 
-                            where num > 50 && num < 70 
-                                select num;   
-
-            // Projections - select
-            var result4 = from num in numbers 
-                            select num * num;
-
-            var result4 = from num in numbers 
-                            where num % 2==0
-                            select num * num;
-            // ordering- order by default ascenduing, orderby descending
-            var ascending=from num in numbers   
-                            orderby num
-                             select num;
-
-            var descending = from num in numbers
-                                orderby num descending
-                                select num;
-            // partioning (take,skip is available only for  method syntax)
-            var partition= numbers.Skip(5).Take(5);
-
-            var result7= numbers.Any(num => num % 2==0);
+         
+         Country country = new Country();       
+         country.LearnLinqOnComplexCollection();   
+           
         }
+    
     }
+  
 }
